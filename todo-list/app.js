05e138419e8db86106e9624ab8c212c2ee247e1e
newush <ushneway@gmail.com>
2020-12-18 16:46:43 -0500

    feat: add handlebars

--- todo-list/app.js
+++ todo-list/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
+const exphbs = require('express-handlebars')
 mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 
@@ -12,10 +12,11 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb, connected!')
 })
-
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
