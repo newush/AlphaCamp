9ac0254732ee300c98dba74370b2fe3bc12a91d6
newush <ushneway@gmail.com>
2020-12-18 15:24:13 -0500

    feat: add mongoose

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,6 +1,17 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb, connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
