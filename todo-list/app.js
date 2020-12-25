3d1a9c3d54f7ebcbf584665ec8e532b2db4c6d85
newush <ushneway@gmail.com>
2020-12-24 23:26:31 -0500

    refactor: modify express routes

--- todo-list/app.js
+++ todo-list/app.js
@@ -5,10 +5,9 @@ const app = express()
 const exphbs = require('express-handlebars')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-const Todo = require('./modules/todo')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const todo = require('./modules/todo')
+const routes = require('./routes')
 
 db.on('error', () => {
   console.log('mongodb error')
@@ -20,57 +19,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-// 設定首頁路由
-app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos: todos }))
-    .catch(error => console.error(error))
-})
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-app.put('/todos/:id/', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-app.delete('/todos/:id/', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
