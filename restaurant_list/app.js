05e438d582182f98c2789a438823910e5e06b862
newush <ushneway@gmail.com>
2020-12-25 15:34:54 -0500

    refactor: modify mongoose config

--- restaurant_list/app.js
+++ restaurant_list/app.js
@@ -2,21 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-const db = mongoose.connection
 const bodyParser = require('body-parser')
 const hbsHelpers = require('./helpers/handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.set('view engine', 'handlebars')
 
--- restaurant_list/models/seeds/restaurantSeeder.js
+++ restaurant_list/models/seeds/restaurantSeeder.js
@@ -1,10 +1,6 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
+
 db.once('open', () => {
   console.log('mongodb connected!')
   const restaurants = [
