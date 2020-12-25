8530f2981d5e7e411fb4644f985f8c3a9cce36e3
newush <ushneway@gmail.com>
2020-12-25 13:45:41 -0500

    refactor: modify express routes

--- restaurant_list/app.js
+++ restaurant_list/app.js
@@ -4,11 +4,10 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const db = mongoose.connection
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const hbsHelpers = require('./helpers/handlebars')
 const methodOverride = require('method-override')
-
+const routes = require('./routes')
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 db.on('error', () => {
@@ -28,75 +27,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.error(error))
-})
-
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  Restaurant.find(
-    {
-      $or: [
-        { name: { $regex: keyword, $options: 'i' } },
-        { category: { $regex: keyword, $options: 'i' } }
-      ]
-    })
-    .lean()
-    .then((restaurants) => {
-      res.render('index', { restaurants, keyword })
-    })
-    .catch(error => console.log(error))
-})
-
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-app.get('/restaurants/:id', (req, res) => {
-  Restaurant.findById(req.params.id)
-    .lean()
-    .then((restaurant) => {
-      res.render('show', { restaurant })
-    })
-    .catch(error => console.log(error))
-})
-
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.post('/restaurants', (req, res) => {
-  return Restaurant.create(req.body)
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant = Object.assign(restaurant, req.body)
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`express is listening on localhost:${port}`)
