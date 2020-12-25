013fc3746f6bcae02d3106c8f5aefc1262d49d95
newush <ushneway@gmail.com>
2020-12-25 10:43:46 -0500

    refactor: modify mongoose config

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,20 +1,13 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
 const exphbs = require('express-handlebars')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb, connected!')
-})
+const app = express()
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
