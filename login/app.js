5f1114575b691e281d52b1f2701be983093df2cb
newush <ushneway@gmail.com>
2020-12-26 20:59:51 -0500

    feat: add body parser

--- login/app.js
+++ login/app.js
@@ -2,17 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
-})
-
+const bodyParser = require('body-parser')
 const users = [
   {
     firstName: 'Tony',
@@ -40,3 +30,20 @@ const users = [
     password: 'password'
   }
 ]
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+
+  console.log('req.body', req.body.email)
+  res.render('index')
+})
+
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
